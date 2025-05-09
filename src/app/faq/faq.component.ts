5dfa9a770ad7c7b9aa55c43745f4ee9191cfaf57
ManhongYuan <manhong.yuan@pushtopic.com>
2025-05-08 23:52:14 -0400

    update faq home page

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -214,13 +214,52 @@ export class FaqComponent implements OnInit {
     this.isSearchOpen = false;
 
     setTimeout(() => {
+<<<<<<< HEAD
       const idx = this.filteredFAQ.findIndex(f => f.question === item.question);
       if (idx >= 0) {
         this.panels.toArray()[idx].open();
         this.panelEls.toArray()[idx].nativeElement
             .scrollIntoView({ behavior: 'smooth', block: 'start' });
+=======
+      const idx = this.filteredFAQ.findIndex(
+        (f) => f.question === item.question
+      );
+      if (idx >= 0) {
+        this.panels.toArray()[idx].open();
+        this.panelEls.toArray()[idx].nativeElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
       }
     });
   }
-  
+
+  get showHome(): boolean {
+    return (
+      !this.currentCategory && !this.currentSubCategory && !this.searchQuery
+    );
+  }
+
+  handleTrendingSelect(item: {
+    question: string;
+    category: string;
+    subCategory: string | null;
+  }) {
+    this.currentCategory = item.category;
+    this.currentSubCategory = '';
+
+    setTimeout(() => {
+      const idx = this.filteredFAQ.findIndex(
+        (f) => f.question === item.question
+      );
+      if (idx >= 0) {
+        this.panels.toArray()[idx].open();
+        this.panelEls.toArray()[idx].nativeElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+>>>>>>> 055c221 (update faq home page)
+      }
+    });
+  }
 }
